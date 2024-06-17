d49aebeac8ff4a8cb91c37f5acee3f3c4891af81
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2024-06-16 21:39:59 -0300

    feat[#3] - Conexão com o banco de dados e continuação da implementação do cadastro de usuario.

--- petshop/src/main/java/br/com/imd/petshop/Service/UsuarioService.java
+++ petshop/src/main/java/br/com/imd/petshop/Service/UsuarioService.java
@@ -1,43 +1,39 @@
 package br.com.imd.petshop.Service;
 
 import br.com.imd.petshop.Entity.Usuario;
+import br.com.imd.petshop.Repository.CepRepository;
 import br.com.imd.petshop.Repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
-
-import java.sql.Date;
 import java.time.ZoneId;
 
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private final CepRepository cepRepository;
 
     @Autowired
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, CepRepository cepRepository) {
         this.usuarioRepository = usuarioRepository;
+        this.cepRepository = cepRepository;
     }
 
     public void cadastrarUsuario(Usuario usuario) {
-        // Obtém a data atual
+        String cepValue = usuario.getCep().getCep();
+        if (!cepRepository.existsById(cepValue)) {
+            cepRepository.save(usuario.getCep());
+        }
         LocalDate dataAtual = LocalDate.now();
-
-        // Converte a data de nascimento para LocalDate
         LocalDate dataNascimento = usuario.getDataDeNascimento().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
-        // Calcula a diferença entre as datas
         Period periodo = Period.between(dataNascimento, dataAtual);
 
-        // Obtém a idade a partir do período
         int idade = periodo.getYears();
-
-        // Define a idade do usuário
         usuario.setIdade(idade);
-
-        // Salva o usuário no banco de dados
         usuarioRepository.save(usuario);
     }
 }
