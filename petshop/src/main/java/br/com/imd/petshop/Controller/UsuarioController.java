c92cb501cdf771400b34a1c679f90c9864b7835d
BIANCA ANTONELLY MEDEIROS DOS SANTOS <100948110+BiancaAntonelly@users.noreply.github.com>
2024-06-20 19:07:14 -0300

    fix [3-CRUD-USUARIO] - Implementado regras de negocio em usuario.

--- petshop/src/main/java/br/com/imd/petshop/Controller/UsuarioController.java
+++ petshop/src/main/java/br/com/imd/petshop/Controller/UsuarioController.java
@@ -1,11 +1,18 @@
 package br.com.imd.petshop.Controller;
 
-import br.com.imd.petshop.Entity.Usuario;
+import br.com.imd.petshop.DTO.UsuarioDTO;
 import br.com.imd.petshop.Service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @CrossOrigin("*")
@@ -21,14 +28,29 @@ public class UsuarioController {
 
     @GetMapping("/cadastro")
     public String cadastro() {
-        return "cadastrar-usuario";
+        return "cadastrar-usuario-cliente";
     }
 
     @PostMapping("/cadastro-usuario")
-    public String cadastroUsuario(@RequestBody Usuario usuario) {
-        usuarioService.cadastrarUsuario(usuario);
-        return "redirect:/usuario/login";
+    public ResponseEntity<Map<String, Object>> cadastrarUsuario(UsuarioDTO usuarioDto) {
+        ResponseEntity<Map<String, Object>> validationResponse = usuarioService.validarCamposObrigatorios(usuarioDto);
+
+        if (validationResponse.getStatusCode() != HttpStatus.OK) {
+            return ResponseEntity.badRequest().body(validationResponse.getBody());
+        }
+
+        try {
+            usuarioService.validarUsuario(usuarioDto);
+        } catch (IllegalArgumentException e) {
+            // Tratar a exceção aqui, por exemplo, retornando um ResponseEntity com os erros
+            Map<String, Object> response = new HashMap<>();
+            response.put("errors", Collections.singletonList(e.getMessage()));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        return null;
     }
+
 
     @GetMapping("/login")
     public String login() {
