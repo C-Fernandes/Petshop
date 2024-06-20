5be785bf447a3bb83386ff9784594f37f207371d
Vanessa-Maria2 <vanessa.silva5205@gmail.com>
2024-06-20 11:12:08 -0300

    feat: implementado o método de adicionar produto no carrinho e atualizar o total. #4

--- petshop/src/main/resources/static/scripts/carrinho.js
+++ petshop/src/main/resources/static/scripts/carrinho.js
@@ -1,15 +1,46 @@
 var total = 0;
 var carrinho = []
 
-function removerProduto() {
+function addProduto(nome, preco, id, qtd) {
+    if (produtoExisteNoCarrinho(id)) {
+        atualizarCarrinho()
+    } else {
+        qtd = 1;
 
-}
+        var produto = {
+            nome: nome,
+            preco: preco,
+            id: id,
+            qtd: qtd
+        };
 
-function addProduto(produto) {
-    console.log(produto)
-    console.log(produto.nome)
+        carrinho.push(produto);
+        atualizarCarrinho();
+    }
 }
 
 function atualizarCarrinho() {
+    total = 0;
+
+    carrinho.forEach(function(produto) {
+       total += Number(produto.preco) * produto.qtd;
+    });
+
+    //atualizar interface
+    console.log('Carrinho atualizado:', carrinho);
+    console.log('Total:', total);
+}
+
+function produtoExisteNoCarrinho(id) {
+    const produtoEncontrado = carrinho.find(produto => produto.id === id);
+    if (produtoEncontrado) {
+        produtoEncontrado.qtd++;
+        return produtoEncontrado;
+    }
+    console.log(carrinho)
+    return undefined;
+}
+
+function removerProduto() {
 
 }
