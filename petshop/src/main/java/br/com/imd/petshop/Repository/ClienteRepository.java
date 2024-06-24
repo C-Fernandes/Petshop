f87c1ed523a57cf198eb30ebd8444d5707e0e388
= <vanessa.silva5205@gmail.com>
2024-06-24 01:34:19 -0300

    fix: correção de conflito. #4

--- petshop/src/main/java/br/com/imd/petshop/Repository/ClienteRepository.java
+++ petshop/src/main/java/br/com/imd/petshop/Repository/ClienteRepository.java
@@ -76,6 +76,63 @@ public class ClienteRepository {
         return cliente;
     }
 
+
+    public List<ClienteDTO> listarClientesComUsuarios() {
+        String sql = "SELECT c.usuario_email, c.qtd_pontos, u.nome, u.telefone, u.data_nascimento FROM cliente c INNER JOIN usuario u ON c.usuario_email = u.email WHERE u.active = true";
+        List<ClienteDTO> clientes = new ArrayList<>();
+        try (Connection conn = DataBaseConfig.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                ClienteDTO clienteDTO = new ClienteDTO();
+                clienteDTO.setEmail(rs.getString("usuario_email"));
+                clienteDTO.setQtdPontos(rs.getLong("qtd_pontos"));
+                clienteDTO.setNome(rs.getString("nome"));
+                clienteDTO.setTelefone(rs.getString("telefone"));
+                clienteDTO.setDataNascimento(rs.getDate("data_nascimento"));
+                clientes.add(clienteDTO);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return clientes;
+    }
+
+    public ClienteDTO findClienteDTOByEmail(String email) {
+        String sql = "SELECT u.nome, u.telefone, u.data_nascimento, u.logradouro, u.numero, u.bairro, u.cep, c.cidade, c.estado " +
+                "FROM cliente l " +
+                "INNER JOIN usuario u ON l.usuario_email = u.email " +
+                "LEFT JOIN cep c ON u.cep = c.cep " +
+                "WHERE u.email = ?";
+        try (Connection conn = DataBaseConfig.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, email);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    ClienteDTO clienteDTO = new ClienteDTO();
+                    clienteDTO.setEmail(email);
+                    clienteDTO.setNome(rs.getString("nome"));
+                    clienteDTO.setTelefone(rs.getString("telefone"));
+                    clienteDTO.setDataNascimento(rs.getDate("data_nascimento"));
+                    clienteDTO.setLogradouro(rs.getString("logradouro"));
+                    clienteDTO.setNumero(rs.getLong("numero"));
+                    clienteDTO.setBairro(rs.getString("bairro"));
+
+                    Cep cep = new Cep();
+                    cep.setCep(rs.getString("cep"));
+                    cep.setCidade(rs.getString("cidade"));
+                    cep.setEstado(rs.getString("estado"));
+
+                    clienteDTO.setCep(cep);
+                    return clienteDTO;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     public Cliente mapRowUsuario(ResultSet rs) throws SQLException {
         Cliente cliente = new Cliente();
         Usuario usuario = new Usuario();
@@ -102,6 +159,42 @@ public class ClienteRepository {
         cliente.setUsuario(usuario);
 
         return cliente;
+    }
+
+    public void atualizarCliente(UsuarioDTO usuarioDTO) {
+        String updateUsuarioSql = "UPDATE usuario SET nome = ?, telefone = ?, logradouro = ?, numero = ?, bairro = ? WHERE email = ?";
+        String updateCepUsuarioSql = "UPDATE usuario SET cep = ? WHERE email = ?";
+        String updateCepSql = "UPDATE cep SET cidade = ?, estado = ? WHERE cep = ?";
+
+        try (Connection conn = DataBaseConfig.getConnection()) {
+            // Atualiza os dados do usuário na tabela usuario
+            try (PreparedStatement ps = conn.prepareStatement(updateUsuarioSql)) {
+                ps.setString(1, usuarioDTO.getNome());
+                ps.setString(2, usuarioDTO.getTelefone());
+                ps.setString(3, usuarioDTO.getLogradouro());
+                ps.setLong(4, usuarioDTO.getNumero());
+                ps.setString(5, usuarioDTO.getBairro());
+                ps.setString(6, usuarioDTO.getEmail());
+                ps.executeUpdate();
+            }
+
+            // Atualiza o cep do usuário na tabela usuario
+            try (PreparedStatement ps = conn.prepareStatement(updateCepUsuarioSql)) {
+                ps.setString(1, usuarioDTO.getCep().getCep());
+                ps.setString(2, usuarioDTO.getEmail());
+                ps.executeUpdate();
+            }
+
+            // Atualiza o cep na tabela cep
+            try (PreparedStatement ps = conn.prepareStatement(updateCepSql)) {
+                ps.setString(1, usuarioDTO.getCep().getCidade());
+                ps.setString(2, usuarioDTO.getCep().getEstado());
+                ps.setString(3, usuarioDTO.getCep().getCep());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException("Erro ao atualizar cliente: " + e.getMessage(), e);
+        }
     }
 
 }
--- petshop/src/main/java/br/com/imd/petshop/Service/UsuarioService.java
+++ petshop/src/main/java/br/com/imd/petshop/Service/UsuarioService.java
@@ -21,7 +21,10 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class UsuarioService {
